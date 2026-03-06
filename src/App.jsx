025d1f6af17b16a058f7e77eec26eed09acbf9c1
Piyush Patil <piyushjpatil46@gmail.com>
2026-03-06 19:00:37 +0530

    Hero section is  const remember

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Hero from './components/Hero'
+import { Hero } from './components/Hero'
 import { Navbar } from './components/Navbar'
 import About from './components/About'
 import Projects from './components/Projects'
